fc952362a4f744abb8cf3929ac71e752edc3b49b
ZAnue9 <uap4_si15p@icloud.com>
2024-11-30 18:31:44 +0900

    kadai_026を追加

---
+++ kadai_026/Jyanken_Chapter26.java
@@ -0,0 +1,53 @@
+package text.kadai_026;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter26 {
+	public String getMyChoice() {
+		System.out.println("自分のじゃんけんの手を入力しましょう");
+		System.out.println("グーはrockのrを入力しましょう");
+		System.out.println("チョキはscissorsのsを入力しましょう");
+		System.out.println("パーはpaperのpを入力しましょう");
+		Scanner scanner = new Scanner(System.in);
+		boolean notStarted = true;
+		while (notStarted) {
+			String hand = scanner.next();
+
+			if (hand.equals("r") || hand.equals("s") || hand.equals("p")) {
+				notStarted = false;
+				scanner.close();
+				return hand;
+			} else {
+				System.out.println("入力された値が正しくありません");
+			}
+		}
+		scanner.close();
+		return null;
+	};
+
+	public String getRandom() {
+		String[] hands = { "r", "s", "p" };
+		int index = (int) Math.round(Math.random() * 3);
+		return hands[index];
+	}
+
+	public void playGame(String myHand, String botHand) {
+		HashMap<String, String> handsMap = new HashMap<>();
+		handsMap.put("r", "グー");
+		handsMap.put("s", "チョキ");
+		handsMap.put("p", "パー");
+		System.out.println("自分の手は " + handsMap.get(myHand) + "、対戦相手の手は" + handsMap.get(botHand));
+
+		if (myHand.equals(botHand)) {
+			System.out.println("あいこです");
+		} else if ((myHand.equals("r") && botHand.equals("s"))
+				|| (myHand.equals("s") && botHand.equals("p"))
+				|| (myHand.equals("p") && botHand.equals("r"))) {
+			System.out.println("自分の勝ちです");
+		} else {
+			System.out.println("自分の負けです");
+		}
+	}
+
+}
